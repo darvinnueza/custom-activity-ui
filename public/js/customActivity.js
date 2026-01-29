323f7b97a0754408944b8f014176c307e74a70b8
DARVIN <4372283+darvinnueza@users.noreply.github.com>
2026-01-29 17:51:13 -0500

    commit
    commit

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,10 +1,28 @@
 /* global Postmonger */
 const connection = new Postmonger.Session();
 
-connection.on('initActivity', function () {
-    console.log('initActivity');
+// Journey pide inicializar la activity
+connection.on('initActivity', function (activity) {
+    console.log('initActivity', activity);
+
+    // IMPORTANTE: decir que la UI está lista
+    connection.trigger('ready');
 });
 
+// Cuando el usuario da click en "Guardar"
+connection.on('clickedSave', function () {
+    console.log('clickedSave');
+
+    // Guardar algo mínimo (aunque sea vacío)
+    const payload = {
+        arguments: {},
+        metaData: {}
+    };
+
+    connection.trigger('updateActivity', payload);
+});
+
+// Cuando Journey quiere cerrar
 connection.on('requestedTokens', function () {
     connection.trigger('ready');
 });
