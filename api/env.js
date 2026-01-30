90297488cbb4e40418d4b2df75e8dfd0289ee450
DARVIN <4372283+darvinnueza@users.noreply.github.com>
2026-01-30 11:51:58 -0500

    commit
    commit

--- api/env.js
+++ api/env.js
@@ -1,11 +1,7 @@
 export default function handler(req, res) {
-    res.setHeader("Content-Type", "application/javascript");
-
-    res.status(200).send(`
-        window.__ENV__ = {
-        API_BASE_URL: "${process.env.API_BASE_URL}",
-        DIVISION_ID: "${process.env.DIVISION_ID}",
-        INTERNAL_TOKEN: "${process.env.INTERNAL_TOKEN}"
-        };
-    `);
+    res.status(200).json({
+        API_BASE_URL: process.env.API_BASE_URL,
+        DIVISION_ID: process.env.DIVISION_ID,
+        INTERNAL_TOKEN: process.env.INTERNAL_TOKEN
+    });
 }
