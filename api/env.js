7c867ded8a3a02adce09fac52b5bd5a3aba9aa95
DARVIN <4372283+darvinnueza@users.noreply.github.com>
2026-02-02 03:45:20 -0500

    commit
    commit

--- api/env.js
+++ api/env.js
@@ -2,6 +2,6 @@ export default function handler(req, res) {
     res.status(200).json({
         API_BASE_URL: process.env.API_BASE_URL,
         DIVISION_ID: process.env.DIVISION_ID,
-        INTERNAL_TOKEN: process.env.INTERNAL_TOKEN
+        INTERNAL_TOKEN: process.env.INTERNAL_TOKEN,
     });
 }
